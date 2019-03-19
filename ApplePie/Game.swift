ef5f6b0739b1110f5763e279fe700650ffc2132f
Dharam Shah <student6@student6s-macbook-air.local>
2019-03-19 12:29:28 -0500

    Initial Commit

---
+++ ApplePie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Dharam Shah on 3/19/19.
+//  Copyright © 2019 Dharam Shah. All rights reserved.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesremaining: Int
+}
